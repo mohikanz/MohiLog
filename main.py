c3b4605495f9f4d0e59197def62d76e6165e9d4f
udgw <takuma.udagawa@gmail.com>
2018-02-23 13:43:30 +0900

    処理の簡略化

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MohiLogger(object):
     template = None
     channel_dict = None
     user_dict = None
+    emoji_dict = None
 
     def __init__(self, **kwargs):
         """
@@ -24,29 +25,30 @@ class MohiLogger(object):
 
         self.slack = Slacker(slack_token)
         self.slack2 = Slacker(slack_token2)
-        self.channel_dict = self.get_channel_dict()
-        self.user_dict = self.get_user_dict()
-        self.user_reverse_dict = self.get_user_reverse_dict()
+        self.channel_dict = self._get_channel_dict()
+        self.user_dict = self._get_user_dict()
+        self.user_reverse_dict = self._get_user_reverse_dict()
+        self.emoji_dict = self._get_emoji_dict()
 
         # template
         env = Environment(loader=FileSystemLoader('.'))
         self.template = env.get_template(self.template_name)
 
-    def get_channel_dict(self):
+    def _get_channel_dict(self):
         """
             {チャネル名:  ID}の辞書を返す。
         """
         res = self.slack.channels.list(exclude_archived=1)
         return {d['name']: d['id'] for d in res.body['channels']}
 
-    def get_user_dict(self):
+    def _get_user_dict(self):
         """
             {ユーザ名: (id, icon_url,)}の辞書を返す。
         """
         res = self.slack.users.list()
         return {m['name']: (m['id'], m['profile']['image_32']) for m in res.body['members']}
 
-    def get_user_reverse_dict(self):
+    def _get_user_reverse_dict(self):
         """
            {ユーザID: 名前} の辞書を返す。
         """
@@ -56,7 +58,7 @@ class MohiLogger(object):
             d[id] = username
         return d
 
-    def get_emoji_dict(self):
+    def _get_emoji_dict(self):
         """"
             {絵文字: url}の辞書を返す。
         """
@@ -74,16 +76,16 @@ class MohiLogger(object):
     def get_channel_log(self, channel):
         """
             チャンネルのログを取得する
+            その際にユーザの名前、emojiのURLを埋め込む。
         """
         channel_id = self.channel_dict[channel]
-        emoji_dict = self.get_emoji_dict()
         messages = self.get_channel_history(channel_id)
         for message in messages:
             user_id = message.get('user')
             username = self.user_reverse_dict.get(user_id) or message.get('username')
             for reaction in message.get('reactions', []):
                 emoji_name = reaction['name']
-                reaction['url'] = emoji_dict.get(emoji_name)
+                reaction['url'] = self.emoji_dict.get(emoji_name, '')
                 users_name = []
                 for user_id in reaction['users']:
                     users_name.append(self.user_reverse_dict.get(user_id))
